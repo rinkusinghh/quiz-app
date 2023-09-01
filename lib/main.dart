72c30bec87ee8a4f7624a90c1c72fbd6a2037da0
rinku singh <rinkusingh05011999@gmail.com>
2023-09-02 04:42:11 +0530

    adding icons to buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 34, 201, 207),
+              Color.fromARGB(255, 34, 220, 186),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
+  ));
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Flutter easy to learn!',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Quiz App')),
+        ],
+      ),
+    );
+  }
+}
