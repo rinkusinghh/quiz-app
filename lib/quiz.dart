cd58bd7722b688bb68fd2e5574c1e462dc4a90ff
rinku singh <rinkusingh05011999@gmail.com>
2023-09-04 04:16:08 +0530

    adding a Statefull Widget

---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 34, 201, 207),
+                Color.fromARGB(255, 34, 220, 186),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
