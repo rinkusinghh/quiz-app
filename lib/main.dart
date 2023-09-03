9cd6d4e5034ebcc98c26be7d91e362de2ffab3dd
rinku singh <rinkusingh05011999@gmail.com>
2023-09-04 04:15:24 +0530

    adding a statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 34, 201, 207),
-              Color.fromARGB(255, 34, 220, 186),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(
+    Quiz(),
+  );
 }
