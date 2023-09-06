9b743c4dc75b04620741bf13b5024ac624541eef
rinku singh <rinkusingh05011999@gmail.com>
2023-09-07 04:39:36 +0530

    Accessing list elements and object properties

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answers_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,20 +13,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Ask question!',
-              style: TextStyle(color: Colors.white, fontSize: 20)),
+          Text(currentQuestion.text,
+              style: const TextStyle(color: Colors.white, fontSize: 16)),
           const SizedBox(
             height: 20,
           ),
-          AnswersButton(answerText: 'answer-1', onTap: () {}),
-          AnswersButton(answerText: 'answer-2', onTap: () {}),
-          AnswersButton(answerText: 'answer-3', onTap: () {}),
-          AnswersButton(answerText: 'answer-4', onTap: () {}),
+          AnswersButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswersButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswersButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswersButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
